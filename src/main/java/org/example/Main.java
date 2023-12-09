16ab2826981a77ada8247449814172efcc4e28c7
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-09 18:37:17 +0100

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,21 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] tab1 = new int[4]; //delkaracja tablicy typu int o długości 4
+        int[] tab2 = {1, 2, 3, 4, 5};//deklaracja tablicy intów o długości 5, i inicjalizacja tablicy
+        tab1[0] = 5;//inicjalizacja (przypisanie wartości) tablicy
+        tab1[3] = 7;//inicjalizacja (przypisanie wartości) tablicy
+
+        System.out.println("Element pierwszy tablicy tab1: " + tab1[0] +
+                            "\nElement drugi tablicy tab1: " + tab1[1] +
+                            "\nElement trzeci tablicy tab1: " + tab1[2] +
+                            "\nElement czwarty tablicy tab1: " + tab1[3] );
+        System.out.println(tab1.length);
+
+        System.out.println("Początek pętli");
+        for(int liczba : tab2){
+            System.out.println("Element tab2: " + liczba);
+        }
+        System.out.println("Koniec pętli");
     }
 }
