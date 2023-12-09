6fc998d074f3a7f26d7188c217d1ebd471081bbb
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-09 20:00:16 +0100

    7_gra_w_kosdtke

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,34 +6,27 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        /* Zadanie 24. */
-        Pracownik pracownik1 = new Pracownik("Anna",
-                                            "Nowak",
-                                            Dzial.IT,
-                                            5_000);//utworzenie nowego obiektu
+        /* Przykład - program symulujący grę w kostkę [1-6] */
+        //metoda random generuje liczby pseudolosowe z zakresu od 0 do 1.
+        Scanner scan = new Scanner(System.in);
 
-        Pracownik pracownik2 = new Pracownik("Tadeusz",
-                                            "Nowicki",
-                                            Dzial.IT,
-                                            7_000);//utworzenie nowego obiektu
+        while(true) {
+            System.out.println("Proszę podać liczbę [ od 1 do 6 ]: ");
+            int podanaCyfra = scan.nextInt();
+            if(podanaCyfra > 0 && podanaCyfra < 7) {
 
-        Kierownik kierownik1 = new Kierownik("Tomasz",
-                                            "Kowalski",
-                                            Dzial.KSIEGOWOSC,
-                                            5_000,
-                                            2_000);
-
-        List<Pracownik> listaPracownikow = new ArrayList<>();
-        listaPracownikow.add(pracownik1);
-        listaPracownikow.add(pracownik2);
-
-        for(Pracownik pracownik : listaPracownikow) {
-            pracownik.wypiszDane();//wywołanie metody pracownik
+                int wylosowanaWartosc = (int) (Math.random() * 5 + 1);  //Metoda random ggenerująca wartości od 1 do 6.
+                if (podanaCyfra == wylosowanaWartosc) {
+                    System.out.println("Gratulację! Wygrałeś! " + "\nPodana cyfra: " + podanaCyfra +
+                            "\nWylosowana cyfra: " + wylosowanaWartosc);
+                } else {
+                    System.out.println("Przegrałeś! Nie udało się :( ! " + "\nPodana cyfra: " + podanaCyfra +
+                            "\nWylosowana cyfra: " + wylosowanaWartosc);
+                }
+            }else{
+                System.out.println("POdałeś złą cyfrę! Podaj ponownie!");
+            }
         }
-
-        kierownik1.wypiszDane();//wywołanie metody pracownik
-
-
 
     }
 }
