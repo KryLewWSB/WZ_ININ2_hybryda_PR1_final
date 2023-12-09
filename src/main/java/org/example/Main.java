f7c2ffb031aeb7860e486151ac8b2d25a8d4cbe3
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-09 18:47:18 +0100

    2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,25 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int[] tab1 = new int[4]; //delkaracja tablicy typu int o długości 4
-        int[] tab2 = {1, 2, 3, 4, 5};//deklaracja tablicy intów o długości 5, i inicjalizacja tablicy
-        tab1[0] = 5;//inicjalizacja (przypisanie wartości) tablicy
-        tab1[3] = 7;//inicjalizacja (przypisanie wartości) tablicy
 
-        System.out.println("Element pierwszy tablicy tab1: " + tab1[0] +
-                            "\nElement drugi tablicy tab1: " + tab1[1] +
-                            "\nElement trzeci tablicy tab1: " + tab1[2] +
-                            "\nElement czwarty tablicy tab1: " + tab1[3] );
-        System.out.println(tab1.length);
+        Scanner scan = new Scanner(System.in);
+        int[] przyjeteLiczby = new int[5];
 
-        System.out.println("Początek pętli");
-        for(int liczba : tab2){
-            System.out.println("Element tab2: " + liczba);
+        for (int i = 0; i < przyjeteLiczby.length; i++){
+            System.out.println("Podaj liczbę numer " + (i + 1));
+            przyjeteLiczby[i] = scan.nextInt();
         }
-        System.out.println("Koniec pętli");
+
+        System.out.println(Arrays.toString(przyjeteLiczby));//1 sposób wyświetloenia danych z tablicy
+
+        //2 sposób wyświetloenia danych z tablicy:
+        for(int liczba : przyjeteLiczby){
+            System.out.println(liczba);
+        }
+
     }
 }
